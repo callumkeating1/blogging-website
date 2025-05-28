fb620489d3a7a7d686d906ddb676efcd4553c7d7
callumkeating1 <CallumKeating124@proton.me>
2025-05-29 08:38:52 +1000

    using eslint to enforce certain rules in code

--- blogging-backend/backend/backend.js
+++ blogging-backend/backend/backend.js
@@ -1,165 +1,132 @@
-const mysql = require('mysql2');
-const bycrpt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const { response } = require('express');
+import mysql from "mysql2";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
 const secretKey = process.env.BACKEND_SECRETKEY;
 const db = mysql.createConnection({
-    host: "localhost",
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: "blog"
+  host: "localhost",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "blog"
 });
 
 db.connect(err => {
-    if (err) {
-        throw err
-    }
-    else {
-        console.log("Connected to MySQL");
-    }
+  if (err) {
+    throw err;
+  }
+  else {
+    console.log("Connected to MySQL");
+  }
 
 });
 
 
 
-
-function generateToken(jsondata, options = {}) {
-    // Make sure jsondata is an object, not an array (you might need to tweak this depending on what you need)
-    if (!jsondata || typeof jsondata !== 'object') {
-        throw new Error('Invalid jsondata. Must be an object.');
-    }
-    const defaultOptions = {
-        issuer: 'blogging-backend',
-        isAdmin: false,
-        ...options,
-    }
-    return jwt.sign(jsondata,secretKey,defaultOptions)
-}
-
-function verifyToken(token) {
-    if (!token) {
-        throw new Error('Token is required');
-    }
-    try {
-        const decoded = jwt.verify(token, secretKey);
-        console.log(decoded);
-        return [0,decoded];
-    } catch (err) {
-        return [1,"Invalid token"];
-    }
-}
-
 async function hashPassword(password, salt = 10) {
-    const hashed = await bycrpt.hash(password,salt);
-    return hashed;
+  const hashed = await bcrypt.hash(password,salt);
+  return hashed;
 }
 async function comparePassword(plainTextPassword,hashedPassword) {
-    return await bycrpt.compare(plainTextPassword,hashedPassword);
-} 
+  return await bcrypt.compare(plainTextPassword,hashedPassword);
+}
 
 async function createaccount(username,email,password) {
-    try{
-        const output = await new Promise((resolve,reject) => {
-            hash = bycrpt.hash(password, 10, (err, hash) => {
-                if (err) throw err;
-                password = hash;
-                db.query('INSERT INTO users (userName, email,password, isAdmin) VALUES (?, ?, ?,0)', [username,email,password],(err, result) => {
-                    if (err) {
-                        reject("server encounted error")
-                    }
-                    console.log("created account");
-                    resolve("successfully created account");
-                });
-            });
-        })
-        return output;
-    }
-    catch (err) {
-        return err;
-    }
+  try{
+    const output = await new Promise((resolve,reject) => {
+      bcrypt.hash(password, 10, (err, hash) => {
+        if (err) throw err;
+        password = hash;
+        db.query("INSERT INTO users (userName, email,password, isAdmin) VALUES (?, ?, ?,0)", [username,email,password],(err) => {
+          if (err) {
+            reject("server encounted error");
+          }
+          console.log("created account");
+          resolve("successfully created account");
+        });
+      });
+    });
+    return output;
+  }
+  catch (err) {
+    return err;
+  }
 }
 async function deleteAccount(username, password) {
-    
-    if (!password || password.trim().length === 0 || !username || username.trim().length === 0) {
-        return "password is blank!";
-    }
 
-    try {
-        const result = await new Promise((resolve, reject) => {
-            db.query('DELETE FROM users WHERE username = ?', [username], (err, result) => {
-                if (err) reject("error");
-                else resolve("success");
-            });
-        });
-        return result;
-    } catch (err) {
-        return err;
-    }
+  if (!password || password.trim().length === 0 || !username || username.trim().length === 0) {
+    return "password is blank!";
+  }
+
+  try {
+    const result = await new Promise((resolve, reject) => {
+      db.query("DELETE FROM users WHERE username = ?", [username], (err) => {
+        if (err) reject("error");
+        else resolve("success");
+      });
+    });
+    return result;
+  } catch (err) {
+    return err;
+  }
 }
 
 
 async function login(username,password) {
-    try {
-        const output = await new Promise((resolve,reject) => {
-            db.query('SELECT id, password, isAdmin FROM users WHERE userName = ?', [username], (err, result) => {
-                if (err) {
-                    console.log(err);
-                    reject("server encounterd an error while logging in");
-                }
-                if (!comparePassword(password,result[0].password)) {
-                    reject("username or password is incorrect");
-                }
-                if (password.length <= 0) {
-                    reject("password is blank!");
-                }
-    
-                let jwtData = {
-                    username: username,
-                    id: result[0].id,
-                    isAdmin: result[0].isAdmin
-                }
-                webToken = jwt.sign(jwtData,secretKey,{algorithm: 'RS256',expiresIn: '30d'});
-                console.log(webToken);
-                resolve(webToken)
-            })
-        })
-        return output;
-    }
-    catch (err) {
-        if (err === "password is blank!") {
-            return "blank password";
+  try {
+    const output = await new Promise((resolve,reject) => {
+      db.query("SELECT id, password, isAdmin FROM users WHERE userName = ?", [username], (err, result) => {
+        if (err) {
+          console.log(err);
+          return reject("server encounterd an error while logging in");
         }
-        else if (err === "username or password is incorrect") {
-            return "username or password incorrect";
+        if (!comparePassword(password,result[0].password)) {
+          return reject("username or password is incorrect");
         }
-    }
+        if (password.length <= 0) {
+          return reject("password is blank!");
+        }
 
+        let jwtData = {
+          username: username,
+          id: result[0].id,
+          isAdmin: result[0].isAdmin
+        };
+        const webToken = jwt.sign(jwtData,secretKey,{ algorithm: "RS256",expiresIn: "30d" });
+        console.log(webToken);
+        resolve(webToken);
+      });
+    });
+    return output;
+  }
+  catch (err) {
+    return err;
+  }
 }
 
 
 function post(username, [title, text]) {
-    db.query('SELECT userID FROM users WHERE userName = ?', [username], (err, result) => {
-        if (err) {
-            return "error occured while fetching userdata";
-        }
-        console.log(result);
-        db.query('INSERT INTO posts (title, content, userID) VALUES (?, ?, ?)', [title, text, result], (err, result) => {
-            if (err) {
-                return "error occured while posting";
-            }
-            else {
-                return "success";
-            }
-        })
-    })
+  db.query("SELECT userID FROM users WHERE userName = ?", [username], (err, result) => {
+    if (err) {
+      return "error occured while fetching userdata";
+    }
+    console.log(result);
+    db.query("INSERT INTO posts (title, content, userID) VALUES (?, ?, ?)", [title, text, result], (err) => {
+      if (err) {
+        return "error occured while posting";
+      }
+      else {
+        return "success";
+      }
+    });
+  });
 }
 async function checkAccountExists(username, email) {
-    return new Promise((resolve, reject) => {
-        db.query('SELECT * FROM users WHERE userName = ? OR email = ?', [username, email], (err, result) => {
-            if (err) return reject(2);
-            resolve(result.length > 0 ? 1 : 0);
-        });
+  return new Promise((resolve, reject) => {
+    db.query("SELECT * FROM users WHERE userName = ? OR email = ?", [username, email], (err, result) => {
+      if (err) return reject(2);
+      resolve(result.length > 0 ? 1 : 0);
     });
+  });
 }
 
 
--- blogging-backend/server.js
+++ blogging-backend/server.js
@@ -1,9 +1,8 @@
-require('dotenv').config();
-const cors = require('cors');
-const express = require('express');
-const http = require('http');
-const app = express();
-const backend = require('./backend/backend.js')
+import "dotenv/config";
+import cors from "cors";
+import { app, express } from "express";
+import http from "http";
+import backend from "./backend/backend.js";
 const port = 5000;
 
 app.use(cors());
@@ -11,39 +10,39 @@ app.use(express.json());
 const server = http.createServer(app);
 
 
-app.get('/', (req,res) => {
-    return res.status(200).send("connected successfully");
+app.get("/", (req,res) => {
+  return res.status(200).send("connected successfully");
 });
-app.post('/account/create', async (req,res) => {
-    const existingAcc = await backend.checkAccountExists(req.body.username, req.body.email);
-    if (existingAcc === 1) {
-        return res.status(409).json({error : 'Conflict', message : 'username or email is already in use'})
-    }
-    const createRresponse = await backend.createaccount(req.body.username,req.body.email,req.body.password);
-    if (createRresponse == "successfully created account")  {
-        return res.status(201).json({error : 'none', message : 'successfully created user', 'code' : 201});
-    }
-    else {
-        console.log(createRresponse)
-    }
+app.post("/account/create", async (req,res) => {
+  const existingAcc = await backend.checkAccountExists(req.body.username, req.body.email);
+  if (existingAcc === 1) {
+    return res.status(409).json({ error : "Conflict", message : "username or email is already in use" });
+  }
+  const createRresponse = await backend.createaccount(req.body.username,req.body.email,req.body.password);
+  if (createRresponse == "successfully created account")  {
+    return res.status(201).json({ error : "none", message : "successfully created user", "code" : 201 });
+  }
+  else {
+    console.log(createRresponse);
+  }
 });
-app.post('/account/login', (req,res) => {
-    if (backend.login(req.body.username,req.body.password)) {
-        return res.status(200).json({ error : 'none', message : 'successfully logged into user', 'code' : 200 });
-    }
-}) 
-app.delete('/account/delete', async (req,res) => {
-    console.log("delete request recived")
-    var response = await backend.deleteAccount(req.body.username,req.body.password);
-    console.log(response)
-    if (response === "error") {
-        return res.status(500).json({error : 'serverError', message : 'server encountered an error deleting user', 'code' : 500});
-    }
-    else {
-        return res.status(200).json({error: 'none', message : 'successfully deleted user', 'code' : 200})
-    }
-})
+app.post("/account/login", (req,res) => {
+  if (backend.login(req.body.username,req.body.password)) {
+    return res.status(200).json({ error : "none", message : "successfully logged into user", "code" : 200 });
+  }
+});
+app.delete("/account/delete", async (req,res) => {
+  console.log("delete request recived");
+  var response = await backend.deleteAccount(req.body.username,req.body.password);
+  console.log(response);
+  if (response === "error") {
+    return res.status(500).json({ error : "serverError", message : "server encountered an error deleting user", "code" : 500 });
+  }
+  else {
+    return res.status(200).json({ error: "none", message : "successfully deleted user", "code" : 200 });
+  }
+});
 
 server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
