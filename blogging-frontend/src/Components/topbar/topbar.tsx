3d1eb7f89ada515e7de4c49f2e712b17c3b63a94
callumkeating1 <CallumKeating124@proton.me>
2025-06-15 21:20:24 +1000

    now using PascalCasing for variable names and added debug message

--- blogging-frontend/src/Components/topbar/topbar.tsx
+++ blogging-frontend/src/Components/topbar/topbar.tsx
@@ -1,15 +1,16 @@
 import DropMenu from "./dropmenu";
 import React, { useState } from 'react';
 export default function Topbar() {
-    const [dropMenuActive, isActive] = useState<boolean>(false);
+    const [DropMenuActive, IsActive] = useState<boolean>(false);
     function toggleMenu() {
-        dropMenuActive ? isActive(false) : isActive(true);
+        DropMenuActive ? IsActive(false) : IsActive(true);
+        console.log("button pressed DropMenuActive is now ", DropMenuActive);
     }
     return (
         <div className="h-fit w-screen bg-gray-300 rounded-3xl p-6 grid grid-cols-9">
         <h1 className="text-8xl place-items-center col-start-4 col-span-3">blogging website</h1>
         <div className="col-start-8 col-span-2 self-center flex justify-end">
-            <button className="text-4xl bg-gray-500 w-fit h-fit rounded-lg p-2">account</button>
+            <button className="text-4xl bg-gray-500 w-fit h-fit rounded-lg p-2" onClick={toggleMenu}>account</button>
         </div>
         </div>
     );
