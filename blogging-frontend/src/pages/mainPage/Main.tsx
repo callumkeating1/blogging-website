e68297c2e06315474d4d8568feb4faaaff540668
callumkeating1 <CallumKeating124@proton.me>
2025-06-18 10:12:57 +1000

    removed secondary file in components file

--- blogging-frontend/src/pages/mainPage/Main.tsx
+++ blogging-frontend/src/pages/mainPage/Main.tsx
@@ -1,8 +1,9 @@
-import Topbar from "../../Components/secondary/topbar"
+import Topbar from "../../Components/topbar"
 export default function MainPage() {
     return (
         <div className='w-screen h-screen flex bg-white'>
             <Topbar />
+            
         </div>
     );
 }
