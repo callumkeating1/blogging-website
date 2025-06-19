d63d770e11005b44d4b61ecec47b0162e0be195b
callumkeating1 <CallumKeating124@proton.me>
2025-06-19 20:26:19 +1000

    fixed eslint error and changed fill-rule to fillRule

--- blogging-frontend/src/Components/topbar/topbar.tsx
+++ blogging-frontend/src/Components/topbar/topbar.tsx
@@ -10,7 +10,7 @@ export default function Topbar() {
             <h1 className="text-2xl font-bold place-items-center col-start-4 col-span-3">blogging website</h1>
             <div className="col-start-8 col-span-2 self-center flex justify-end" >
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="text-sm bg-gray-500 w-fit h-fit rounded-lg p-1" viewBox="0 0 16 16" onClick={toggleMenu}>
-                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+                    <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
                 </svg>
                 {DropMenuActive ? <DropMenu toggleMenu={toggleMenu} /> : null}
             </div>
