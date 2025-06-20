f1f4d12f2e0bf42b4b911eb2a3d98a14d6f34670
callumkeating1 <CallumKeating124@proton.me>
2025-06-20 14:35:28 +1000

    added isclicked context, will give function later

--- blogging-frontend/src/pages/mainPage/Main.tsx
+++ blogging-frontend/src/pages/mainPage/Main.tsx
@@ -1,10 +1,23 @@
-import Topbar from "../../Components/topbar"
+import Topbar from "../../Components/topbar";
 import Contents from "../../Components/contents";
+import { createContext, useState } from "react";
+
+// 👇 Capitalized for correct usage in JSX
+export const IsClickedContext = createContext<boolean>(false);
+
 export default function MainPage() {
+    const [isClicked, setIsClicked] = useState(false);
+
+    const handleClick = () => {
+        setIsClicked(true);
+    };
+
     return (
-        <div className='w-screen h-screen flex bg-white flex-col'>
-            <Topbar />
-            <Contents />
-        </div>
+        <IsClickedContext.Provider value={isClicked}>
+            <div className='w-screen h-screen flex bg-white flex-col' onClick={handleClick}>
+                <Topbar />
+                <Contents />
+            </div>
+        </IsClickedContext.Provider>
     );
 }
