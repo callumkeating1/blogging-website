2753b0de76cf3e1576dded5b101d20917ef7171f
callumkeating1 <CallumKeating124@proton.me>
2025-06-23 05:54:41 +1000

    moved dropmenu to correct area

--- blogging-frontend/src/Components/topbar/dropmenu/dropmenu.tsx
+++ blogging-frontend/src/Components/topbar/dropmenu/dropmenu.tsx
@@ -1,15 +1,23 @@
-import Icon from './item'
-type MyComponentProps = {
+import Icon from './item';
+
+type DropMenuProps = {
   toggleMenu: () => void;
   dropMenuActive: boolean;
 };
-export default function DropMenu({toggleMenu}:MyComponentProps) {
-    return (
-        <div className="fixed top-0 left-0 h-screen w-screen" onClick={toggleMenu}>
-            <div className="fixed top-16 right-8 bg-zinc-500 dark:bg-zinc-700 p-5 text-xl rounded-md ring-1 ring-black"  onClick={e => e.stopPropagation()}>
-                <Icon Text='account' IconUrl='/svgs/dropmenu/account.svg' />
-                <Icon Text='settings' IconUrl='/svgs/dropmenu/settings.svg' />
-            </div>
-        </div>
-    )
+
+export default function DropMenu({ toggleMenu }: DropMenuProps) {
+  return (
+    <div
+      className="fixed inset-0"
+      onClick={toggleMenu}
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="absolute top-16 left-1 w-60 max-w-[90vw] rounded-lg bg-zinc-500 dark:bg-zinc-700 p-4 shadow-lg ring-1 ring-black/10 space-y-3"
+      >
+        <Icon Text="Account" IconUrl="/svgs/dropmenu/account.svg" />
+        <Icon Text="Settings" IconUrl="/svgs/dropmenu/settings.svg" />
+      </div>
+    </div>
+  );
 }
