bfdd68c339fb7ecd02416d147edc663e6f7ec793
callumkeating1 <CallumKeating124@proton.me>
2025-06-23 19:19:52 +1000

    changed width of dropmenu

--- blogging-frontend/src/Components/topbar/dropmenu/item/item.tsx
+++ blogging-frontend/src/Components/topbar/dropmenu/item/item.tsx
@@ -10,7 +10,7 @@ export default function Item(props:ItemProps) {
         <>
             <div className="flex bg-zinc-600 dark:bg-zinc-400 p-1 rounded-lg mb-3 hover:bg-zinc-700 dark:hover:bg-zinc-500 duration-500">
                 <img src={props.IconUrl} alt={props.Text} />
-                <p className="text-sm ml-1">{props.Text}</p>
+                <p className="text-sm font-semibold ml-1">{props.Text}</p>
             </div>
         </>
     )
