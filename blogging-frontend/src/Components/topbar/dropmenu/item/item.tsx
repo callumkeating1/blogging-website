6baa760ef3a42d5d64487821142c7736b2c6b34b
callumkeating1 <CallumKeating124@proton.me>
2025-06-23 10:33:45 +0100

    added darkmode to dropmenu

--- blogging-frontend/src/Components/topbar/dropmenu/item/item.tsx
+++ blogging-frontend/src/Components/topbar/dropmenu/item/item.tsx
@@ -1,14 +1,14 @@
 import { JSX } from "react"
 
 interface ItemProps {
-    IconUrl: string ,
+    IconUrl: string,
     Text: string
 }
 
 export default function Item(props:ItemProps) {
     return (
         <>
-            <div className="flex bg-[#6449b4] hover:bg-[#8370be] dark:bg-zinc-400 p-1 rounded-lg mb-3 dark:hover:bg-zinc-500 duration-500">
+            <div className="flex bg-[#6449b4] hover:bg-[#8370be] dark:bg-[#7058c4] dark:hover:bg-zinc-500 p-1 rounded-lg mb-3 duration-500">
                 <img src={props.IconUrl} alt={props.Text} />
                 <p className="text-sm font-semibold ml-1">{props.Text}</p>
             </div>
