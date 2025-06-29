c553296428ae0ad12d5a7e476a34bd9bfd15d708
callumkeating1 <CallumKeating124@proton.me>
2025-06-30 04:31:17 +1000

    fixed issue where website would crash apon dropmenu being triggered due to missing width and height propertys in <Image /> tag

--- blogging-frontend/src/app/Components/topbar/dropmenu/item/item.tsx
+++ blogging-frontend/src/app/Components/topbar/dropmenu/item/item.tsx
@@ -12,7 +12,7 @@ export default function Item(props:ItemProps) {
     return (
         <>
             <div className="flex bg-[hsl(255,42%,50%)] hover:bg-[hsl(255,38%,59%)] dark:bg-[hsl(253,48%,56%)] dark:hover:bg-zinc-500 p-1 rounded-lg mb-3 duration-500">
-                <Image src={props.IconUrl} alt={props.Text} />
+                <Image src={props.IconUrl} alt={props.Text} width={16} height={16}/>
                 <p className="text-sm font-semibold ml-1">{props.Text}</p>
             </div>
         </>
