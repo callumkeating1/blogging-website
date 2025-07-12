c8b3fb1028258fe508c42e75b619d45f715cfc99
callumkeating1 <CallumKeating124@proton.me>
2025-07-12 20:18:59 +1000

    fixed text editor issue caused by useParams not being typed by default

--- blogging-frontend/src/app/account/[user]/page.tsx
+++ blogging-frontend/src/app/account/[user]/page.tsx
@@ -2,17 +2,17 @@
 import { useParams } from 'next/navigation';
 import Contents from '@/app/Components/contents';
 export default function AccountPage() {
-    const router = useParams();
-    const user = router.user;
-    if (user === undefined) {
+    const { user } = useParams() as { user?: string | string[] };
+    if (user === undefined || Array.isArray(user)) {
         return (
             <p>specify a username</p>
         )
     }
+    console.log("user: ", user);
     return (
         <div className='w-screen h-screen dark:bg-[hsl(255,65%,35%)] flexbox flex-row'>
             <p className='bg-purple-600 dark:bg-purple-800 p-5 rounded-lg'>{`Welcome to ${user}'s profile!`}</p>
-            <Contents username={user[0]} /> 
+            <Contents username={user} /> 
         </div>
     )
 }
