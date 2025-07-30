e242fc194279e2d124e586608e2d43706d3b1778
callumkeating1 <CallumKeating124@proton.me>
2025-07-30 22:19:40 +1000

    made signup page look better and made it a form

--- blogging-frontend/src/app/Components/signup/button/signup-button.tsx
+++ blogging-frontend/src/app/Components/signup/button/signup-button.tsx
@@ -6,6 +6,6 @@ interface SignupButtonProps {
 
 export default function SignupButton (props:SignupButtonProps) {
     return (
-        <input type={props.type} placeholder={props.placeholder} className="bg-purple-200 text-gray-900 p-5 rounded-xl hover:bg-purple-300 duration-500"/>
+        <input type={props.type} placeholder={props.placeholder} name={props.placeholder} className="bg-purple-200 text-gray-900 p-5 rounded-xl hover:bg-purple-300 duration-500 h-[10%] lg:h-[20%] w-[90%] "/>
     )
 }
--- blogging-frontend/src/app/Components/signup/list/signup-list.tsx
+++ blogging-frontend/src/app/Components/signup/list/signup-list.tsx
@@ -1,16 +1,29 @@
+'use client';
 import SignupButton from '@/app/Components/signup/button/signup-button'
 
 
 export default function SignupList () {
+    function signup(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault(); // Stop default form submission
+
+        const formData = new FormData(event.currentTarget); // ✅ attach to form
+        const email = formData.get("email");
+        const username = formData.get("username");
+        const password = formData.get("password");
+        console.log(email, "   ", username, "   ", password);
+    }
     return (
         <div className='flex flex-col bg-purple-700 h-screen'>
             <h1 className='self-center text-xl bg-purple-600 mt-2'>sign up</h1>
             <br/>
-            <div className="flex flex-col items-center gap-3">
-                <SignupButton placeholder="email" type="email" />
-                <SignupButton placeholder="username" type="text" />
-                <SignupButton placeholder="password" type="password" />
-            </div>
+            <form onSubmit={signup}>
+                <div className="flex flex-col items-center gap-3 h-screen">
+                    <SignupButton placeholder="email" type="email"/>
+                    <SignupButton placeholder="username" type="text"/>
+                    <SignupButton placeholder="password" type="password"/>
+                    <button className='bg-purple-600 text-white p-2 rounded-md lg:p-4 text-lg duration-500 hover:p-3 lg:hover:p-6 lg:hover:text-xl' type='submit'>sign up!</button>
+                </div>
+            </form>
         </div>
     )
 }
